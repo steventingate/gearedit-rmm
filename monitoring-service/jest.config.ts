bb6a670cc858ef1230e1545a89c60ce0423340bd
steventingate <111670868+steventingate@users.noreply.github.com>
2025-06-27 20:09:35 +1000

    Initial Upload

---
+++ monitoring-service/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'monitoring-service',
+  preset: '../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../coverage/monitoring-service',
+};
---
+++ remote-desktop-service/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'remote-desktop-service',
+  preset: '../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../coverage/remote-desktop-service',
+};
